b444e713e4d05057c1905e7382e6d392a07dc5cc
root <kamilahmed602@gmail.com>
2023-04-23 15:45:16 +0000

    this it will be my last commit

--- kamil.sh
+++ kamil.sh
@@ -2,3 +2,4 @@
 so i have to learn git and became very familer in it
 
 
+echo " kamil is became professinal in  printing and  he bacame  number one in the world in devops   I think you are exsagrating be more realistic and think in proper way  that  it will lead you to be proffisional in the IT fild" 
