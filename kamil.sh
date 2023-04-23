039e0bf151302ce88108d61c7d8f5d6a1c2b2534
root <kamilahmed602@gmail.com>
2023-04-23 15:37:08 +0000

    new change

---
+++ kamil.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+so i have to learn git and became very familer in it
+
+
--- sample.sh
+++ sample.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "kamil ahmed almustfa is devops engineer he is working in the most famoust campany in the world"
 echo "balsam is my belovet doughter"
+echo " adding anthe   third line"
+echo " the calloclation of 20 number "
+
