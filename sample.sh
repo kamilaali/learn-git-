992374e3af81fd150329209692a5384200d1361a
root <kamilahmed602@gmail.com>
2023-04-26 14:37:24 +0000

    git revert changes

--- sample.sh
+++ sample.sh
@@ -3,4 +3,4 @@ echo "kamil ahmed almustfa is devops engineer he is working in the most famoust 
 echo "balsam is my belovet doughter"
 echo " adding anthe   third line"
 echo " the calloclation of 20 number "
-
+echo " git revert in practise "
