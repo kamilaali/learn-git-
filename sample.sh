de7b374aa9ba9ddee12b8a92c54008cd76a0a0b4
kamilaali <kamilahmed602@gmail.com>
2023-05-16 22:48:37 +0000

    changes

--- sample.sh
+++ sample.sh
@@ -2,4 +2,5 @@
 echo "kamil ahmed almustfa is devops engineer he is working in the most famoust campany in the world"
 echo "balsam is my belovet doughter"
 echo " code Freeze for year end "
+echo " code change  add" 
 
